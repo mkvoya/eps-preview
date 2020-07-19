086f83c7c289a08a895f5abb4ac1ea3e74916a9b
Mingkai Dong <mingkaidong@gmail.com>
2020-07-19 22:36:44 +0800

    robustness: add error handling

--- src/extension.ts
+++ src/extension.ts
@@ -25,6 +25,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 		if (!document) {
 			// No active document
+			console.log("No active document. Do nothing.");
 			return;
 		}
 
@@ -34,21 +35,46 @@ export function activate(context: vscode.ExtensionContext) {
 		temp.track();
 		temp.open('eps-preview-pdf', function (pdfErr, pdfInfo) {
 			if (pdfErr) {
+				console.log("Creating temporary file eps-preview-pdf failed.");
 				return;
 			}
 			temp.open('eps-preview-svg', function (svgErr, svgInfo) {
 				if (svgErr) {
+					console.log("Creating temporary file eps-preview-svg failed.");
 					return;
 				}
 				// Transform EPS to SVG
 				// Thank https://superuser.com/a/769466/502597.
-				execSync(`ps2pdf -dEPSCrop - ${pdfInfo.path}`, { input: epsContent });
-				execSync(`pdf2svg ${pdfInfo.path} ${svgInfo.path}`);
-				const stat = fs.fstatSync(svgInfo.fd);
-				let svgContent = Buffer.alloc(stat.size);
-				fs.readSync(svgInfo.fd, svgContent, 0, stat.size, null);
-				// Show SVG in the webview panel
-				panel.webview.html = `<h1>${filename}</h1>` + svgContent;
+				try {
+					execSync(`ps2pdf -dEPSCrop - ${pdfInfo.path}`, { input: epsContent });
+				} catch (err) {
+					vscode.window.showInformationMessage('Failed to execute ps2pdf, is that installed?');
+					console.log("Error executing ps2pdf.");
+					console.log(err);
+					// Clean up
+					temp.cleanupSync();
+					return;
+				}
+				try {
+					execSync(`pdf2svg ${pdfInfo.path} ${svgInfo.path}`);
+				} catch (err) {
+					vscode.window.showInformationMessage('Failed to execute pdf2svg, is that installed?');
+					console.log("Error executing pdf2svg.");
+					console.log(err);
+					// Clean up
+					temp.cleanupSync();
+					return;
+				}
+				try {
+					const stat = fs.fstatSync(svgInfo.fd);
+					let svgContent = Buffer.alloc(stat.size);
+					fs.readSync(svgInfo.fd, svgContent, 0, stat.size, null);
+					// Show SVG in the webview panel
+					panel.webview.html = `<h1>${filename}</h1>` + svgContent;
+				} catch (err) {
+					console.log("Error reading the final file.");
+					console.log(err);
+				}
 			});
 		});
 
