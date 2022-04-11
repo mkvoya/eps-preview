24e56cc272617d054d86b2bc33f6b74af1302d2f
sxn <xiaoniu.sxn@outlook.com>
2022-04-11 14:59:06 +0800

    split preview generation into a separate function

--- src/extension.ts
+++ src/extension.ts
@@ -6,6 +6,63 @@ import temp = require('temp');
 import fs = require('fs');
 import path = require('path');
 
+function genPreview(filename:string, epsContent:string, panel:vscode.WebviewPanel) {
+	// launch.json configuration
+	const config = vscode.workspace.getConfiguration('eps-preview');
+	// retrieve values
+	const ps2pdfPath = config.get('path.ps2pdf', 'ps2pdf');
+	const pdf2svgPath = config.get('path.pdf2svg', 'pdf2svg');
+
+	temp.track();
+	temp.open('eps-preview-pdf', function (pdfErr, pdfInfo) {
+		if (pdfErr) {
+			console.log("Creating temporary file eps-preview-pdf failed.");
+			return;
+		}
+		temp.open('eps-preview-svg', function (svgErr, svgInfo) {
+			if (svgErr) {
+				console.log("Creating temporary file eps-preview-svg failed.");
+				return;
+			}
+			// Transform EPS to SVG
+			// Thank https://superuser.com/a/769466/502597.
+			try {
+				execSync(`${ps2pdfPath} -dEPSCrop - ${pdfInfo.path}`, { input: epsContent });
+			} catch (err) {
+				vscode.window.showInformationMessage('Failed to execute ps2pdf, is that installed?');
+				console.log("Error executing ps2pdf.");
+				console.log(err);
+				// Clean up
+				temp.cleanupSync();
+				return;
+			}
+			try {
+				execSync(`${pdf2svgPath} ${pdfInfo.path} ${svgInfo.path}`);
+			} catch (err) {
+				vscode.window.showInformationMessage('Failed to execute pdf2svg, is that installed?');
+				console.log("Error executing pdf2svg.");
+				console.log(err);
+				// Clean up
+				temp.cleanupSync();
+				return;
+			}
+			try {
+				const stat = fs.fstatSync(svgInfo.fd);
+				let svgContent = Buffer.alloc(stat.size);
+				fs.readSync(svgInfo.fd, svgContent, 0, stat.size, null);
+				// Show SVG in the webview panel
+				panel.webview.html = `<h1>${filename}</h1>` + svgContent;
+			} catch (err) {
+				console.log("Error reading the final file.");
+				console.log(err);
+			}
+		});
+	});
+
+	// Clean up
+	temp.cleanupSync();
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -31,61 +88,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 		const epsContent = document.getText();
 		const filename = path.basename(document.fileName);
-
-		// launch.json configuration
-		const config = vscode.workspace.getConfiguration('eps-preview');
-		// retrieve values
-		const ps2pdfPath = config.get('path.ps2pdf', 'ps2pdf');
-		const pdf2svgPath = config.get('path.pdf2svg', 'pdf2svg');
-
-		temp.track();
-		temp.open('eps-preview-pdf', function (pdfErr, pdfInfo) {
-			if (pdfErr) {
-				console.log("Creating temporary file eps-preview-pdf failed.");
-				return;
-			}
-			temp.open('eps-preview-svg', function (svgErr, svgInfo) {
-				if (svgErr) {
-					console.log("Creating temporary file eps-preview-svg failed.");
-					return;
-				}
-				// Transform EPS to SVG
-				// Thank https://superuser.com/a/769466/502597.
-				try {
-					execSync(`${ps2pdfPath} -dEPSCrop - ${pdfInfo.path}`, { input: epsContent });
-				} catch (err) {
-					vscode.window.showInformationMessage('Failed to execute ps2pdf, is that installed?');
-					console.log("Error executing ps2pdf.");
-					console.log(err);
-					// Clean up
-					temp.cleanupSync();
-					return;
-				}
-				try {
-					execSync(`${pdf2svgPath} ${pdfInfo.path} ${svgInfo.path}`);
-				} catch (err) {
-					vscode.window.showInformationMessage('Failed to execute pdf2svg, is that installed?');
-					console.log("Error executing pdf2svg.");
-					console.log(err);
-					// Clean up
-					temp.cleanupSync();
-					return;
-				}
-				try {
-					const stat = fs.fstatSync(svgInfo.fd);
-					let svgContent = Buffer.alloc(stat.size);
-					fs.readSync(svgInfo.fd, svgContent, 0, stat.size, null);
-					// Show SVG in the webview panel
-					panel.webview.html = `<h1>${filename}</h1>` + svgContent;
-				} catch (err) {
-					console.log("Error reading the final file.");
-					console.log(err);
-				}
-			});
-		});
-
-		// Clean up
-		temp.cleanupSync();
+		genPreview(filename, epsContent, panel);
 	});
 
 	context.subscriptions.push(disposable);
